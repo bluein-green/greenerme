81a33100401a6e91cfe909c15daf1de215750bd4
DLYH <lim.yh.donna@gmail.com>
2018-06-18 12:31:00 +0800

    Managed to add the create acc and stored into firebase

---
+++ app/src/main/java/com/teamturtles/greenerme/CreateAccPage.java
@@ -0,0 +1,104 @@
+package com.teamturtles.greenerme;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class CreateAccPage extends AppCompatActivity {
+
+    ProgressBar progressBar;
+    EditText username_signup, password_signup, email_signup;
+    Button signup_btn;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_acc_page);
+
+        username_signup = (EditText) findViewById(R.id.username_signup);
+        password_signup = (EditText) findViewById(R.id.password_signup);
+        email_signup = (EditText) findViewById(R.id.email_signup);
+        signup_btn = (Button) findViewById(R.id.signup_btn);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressbar);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        signup_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+    }
+
+    private void registerUser() {
+        String username = username_signup.getText().toString().trim();
+        String password = password_signup.getText().toString().trim();
+        String email = email_signup.getText().toString().trim();
+
+        if (username.isEmpty() || password.isEmpty() || password.length() < 6 || email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            if (username.isEmpty()) {
+                username_signup.setError("Email is required");
+                username_signup.requestFocus();
+            }
+
+            if (password.isEmpty()) {
+                password_signup.setError("Password is required");
+                password_signup.requestFocus();
+            }
+
+            if (password.length() < 6) {
+                password_signup.setError("Minimum length of password should be 6");
+                password_signup.requestFocus();
+            }
+
+            if (email.isEmpty()) {
+                email_signup.setError("Email is required");
+                email_signup.requestFocus();
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                email_signup.setError("Please enter a valid email");
+                email_signup.requestFocus();
+            }
+
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                progressBar.setVisibility(View.GONE);
+
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "User Registered Successful", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Some Error Occurred", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void toSignInPage (View view) { // created & send to the sign in page
+        Intent startIntent = new Intent(getApplicationContext(), LoginPage.class);
+        startActivity(startIntent);
+    }
+}
--- app/src/main/java/com/teamturtles/greenerme/HomePage_loggedout.java
+++ app/src/main/java/com/teamturtles/greenerme/HomePage_loggedout.java
@@ -1,7 +1,12 @@
 package com.teamturtles.greenerme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class HomePage_loggedout extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class HomePage_loggedout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page_loggedout);
+
+        Toast.makeText(this, "Hi There! Welcome in!", Toast.LENGTH_SHORT).show();
+
+        final TextView signup_btn = (TextView) findViewById(R.id.signup_btn);
+
+        signup_btn.setOnClickListener(new View.OnClickListener() { // To click to the createe account page
+            @Override
+            public void onClick(View view) {
+                Intent signup_intent = new Intent(HomePage_loggedout.this, CreateAccPage.class);
+                HomePage_loggedout.this.startActivity(signup_intent);
+            }
+        });
+    }
+
+
+
+    /*public void createAccount(View view) { // To click to the create account page
+        Intent startIntent = new Intent(getApplicationContext(), CreateAccPage.class);
+        startActivity(startIntent);
+    }
+    */
+
+    public void loginAccount(View view) {
+        Intent startIntent = new Intent(getApplicationContext(), LoginPage.class);
+        startActivity(startIntent);
     }
 }
