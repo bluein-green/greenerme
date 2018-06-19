d76fa881f398245a05c3beeb72070b5eb84d927f
mj <j.minjia@gmail.com>
2018-06-19 14:11:59 +0800

    added search, quiz, points, account and about app pages; removed header bar

---
+++ app/src/main/java/com/teamturtles/greenerme/ViewAccountPage.java
@@ -0,0 +1,13 @@
+package com.teamturtles.greenerme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewAccountPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_account_page);
+    }
+}
