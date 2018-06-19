03b74091e55ae8a18867f42de11764b349a3e299
mj <j.minjia@gmail.com>
2018-06-19 12:47:05 +0800

    added icon pictures, made the menu page

--- app/src/main/java/com/teamturtles/greenerme/HomePage_loggedin.java
+++ app/src/main/java/com/teamturtles/greenerme/HomePage_loggedin.java
@@ -2,6 +2,7 @@ package com.teamturtles.greenerme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomePage_loggedin extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class HomePage_loggedin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page_loggedin);
+
+        Toast.makeText(this, "Logged in", Toast.LENGTH_SHORT).show();
     }
 }
