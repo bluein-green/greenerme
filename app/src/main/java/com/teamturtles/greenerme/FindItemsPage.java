a30bfd8fd71cda09e5ffe0009b24b4afd247fb97
mj <j.minjia@gmail.com>
2018-06-21 19:36:03 +0800

    added temporary stuff to test out pulling/pushing to firebase, under categories page; refined item class

--- app/src/main/java/com/teamturtles/greenerme/FindItemsPage.java
+++ app/src/main/java/com/teamturtles/greenerme/FindItemsPage.java
@@ -1,13 +1,32 @@
 package com.teamturtles.greenerme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class FindItemsPage extends AppCompatActivity {
+    private Button category_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_items_page);
+
+        // click listeners for categories button
+        category_btn = (Button) findViewById(R.id.FI_categories_btn);
+        category_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCategoryPage();
+            }
+        });
+    }
+
+    public void openCategoryPage() {
+        Intent intent = new Intent(this, CategoriesPage.class);
+        startActivity(intent);
     }
 }
